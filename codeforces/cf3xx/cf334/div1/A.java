777da3932661a13b268f699a83fa6a1eb0d55d31
hama_du <dercll1020@gmail.com>
2016-08-17 20:40:32 +0900

    33-4

---
+++ codeforces/cf3xx/cf334/div1/A.java
@@ -0,0 +1,199 @@
+package codeforces.cf3xx.cf334.div1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+/**
+ * Created by hama_du on 2016/08/17.
+ */
+public class A {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        char[] c = in.nextToken().toCharArray();
+
+        int maxlen = 0;
+        int lc = 0;
+        int cnt = 0;
+        for (int i = 0; i < n ; ) {
+            int j = i;
+            while (j < n && c[i] == c[j]) {
+                j++;
+            }
+            if (maxlen == j-i) {
+                lc++;
+            } else if (maxlen < j-i) {
+                maxlen = j-i;
+                lc = 1;
+            }
+            cnt++;
+            i = j;
+        }
+
+        if (maxlen >= 3 || (maxlen == 2 && lc >= 2)) {
+            cnt += 2;
+        } else if (maxlen >= 2) {
+            cnt++;
+        }
+        out.println(cnt);
+        out.flush();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int[] nextInts(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextInt();
+            }
+            return ret;
+        }
+
+
+        private int[][] nextIntTable(int n, int m) {
+            int[][] ret = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextInt();
+                }
+            }
+            return ret;
+        }
+
+        private long[] nextLongs(int n) {
+            long[] ret = new long[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextLong();
+            }
+            return ret;
+        }
+
+        private long[][] nextLongTable(int n, int m) {
+            long[][] ret = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextLong();
+                }
+            }
+            return ret;
+        }
+
+        private double[] nextDoubles(int n) {
+            double[] ret = new double[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextDouble();
+            }
+            return ret;
+        }
+
+        private int next() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public char nextChar() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            if ('a' <= c && c <= 'z') {
+                return (char) c;
+            }
+            if ('A' <= c && c <= 'Z') {
+                return (char) c;
+            }
+            throw new InputMismatchException();
+        }
+
+        public String nextToken() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.append((char) c);
+                c = next();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public int nextInt() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            long sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble() {
+            return Double.valueOf(nextToken());
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+}
