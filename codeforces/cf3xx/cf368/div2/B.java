7f5f4482d94bf7b68daa9589aaf87e8192a7e28d
hama_du <dercll1020@gmail.com>
2016-08-24 00:15:18 +0900

    cf368

---
+++ codeforces/cf3xx/cf368/div2/B.java
@@ -0,0 +1,220 @@
+package codeforces.cf3xx.cf368.div2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+/**
+ * Created by hama_du on 2016/08/22.
+ */
+public class B {
+    private static final int INF = (int)1e9+10;
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int k = in.nextInt();
+        int[][][] graph = buildWeightedGraph(in, n, m);
+        boolean[] hasStorage = new boolean[n];
+        for (int i = 0; i < k ; i++) {
+            hasStorage[in.nextInt()-1] = true;
+        }
+        int min = INF;
+        for (int i = 0; i < n ; i++) {
+            if (hasStorage[i]) {
+                for (int[] e : graph[i]) {
+                    if (!hasStorage[e[0]]) {
+                        min = Math.min(min, e[1]);
+                    }
+                }
+            }
+        }
+        out.println(min == INF ? -1 : min);
+        out.flush();
+    }
+
+    static int[][][] buildWeightedGraph(InputReader in, int n, int m) {
+        int[][] edges = new int[m][];
+        int[][][] graph = new int[n][][];
+        int[] deg = new int[n];
+        for (int i = 0 ; i < m ; i++) {
+            int a = in.nextInt()-1;
+            int b = in.nextInt()-1;
+            int w = in.nextInt();
+            deg[a]++;
+            deg[b]++;
+            edges[i] = new int[]{a, b, w};
+        }
+        for (int i = 0 ; i < n ; i++) {
+            graph[i] = new int[deg[i]][2];
+        }
+        for (int i = 0 ; i < m ; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int w = edges[i][2];
+            graph[a][--deg[a]][0] = b;
+            graph[b][--deg[b]][0] = a;
+            graph[a][deg[a]][1] = w;
+            graph[b][deg[b]][1] = w;
+        }
+        return graph;
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int[] nextInts(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextInt();
+            }
+            return ret;
+        }
+
+
+        private int[][] nextIntTable(int n, int m) {
+            int[][] ret = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextInt();
+                }
+            }
+            return ret;
+        }
+
+        private long[] nextLongs(int n) {
+            long[] ret = new long[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextLong();
+            }
+            return ret;
+        }
+
+        private long[][] nextLongTable(int n, int m) {
+            long[][] ret = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextLong();
+                }
+            }
+            return ret;
+        }
+
+        private double[] nextDoubles(int n) {
+            double[] ret = new double[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextDouble();
+            }
+            return ret;
+        }
+
+        private int next() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public char nextChar() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            if ('a' <= c && c <= 'z') {
+                return (char) c;
+            }
+            if ('A' <= c && c <= 'Z') {
+                return (char) c;
+            }
+            throw new InputMismatchException();
+        }
+
+        public String nextToken() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.append((char) c);
+                c = next();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public int nextInt() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public long nextLong() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            long sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public double nextDouble() {
+            return Double.valueOf(nextToken());
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+}
--- todo.md
+++ todo.md
@@ -1,18 +1,8 @@
 ## 解けてない
 
-(cs1-e)
-https://csacademy.com/contest/archive/#task/colored_marbles  (原因不明WA)
-
-(cs9-f)
-https://csacademy.com/contest/round-9/#task/101-palindromes (原因不明WA)
-
 (SRM696 div1easy,div1med)
-
-(CF367-div2e)
+https://csacademy.com/contest/archive/#task/colored_marbles  (原因不明WA)
+https://csacademy.com/contest/round-9/#task/101-palindromes (原因不明WA)
 http://codeforces.com/contest/706/problem/E (定数倍TLE,やる気なし)
-
-(Aimtech-d)
 http://codeforces.com/contest/623/problem/D (やり方分からん)
-
-(AGC003-E)
 http://agc003.contest.atcoder.jp/tasks/agc003_e (no-idea)
