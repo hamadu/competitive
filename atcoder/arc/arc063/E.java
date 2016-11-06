dfb0a09a04b9232f27ed142d09d3dcb73b9d601a
hama_du <dercll1020@gmail.com>
2016-11-07 00:12:36 +0900

    arc063

---
+++ atcoder/arc/arc063/E.java
@@ -0,0 +1,267 @@
+package atcoder.arc.arc063;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class E {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int[][] graph = buildGraph(in, n, n-1);
+
+
+        boolean[] add = new boolean[n];
+        int[] num = new int[n];
+        Arrays.fill(num, -1);
+
+        Queue<State> q = new PriorityQueue<>();
+
+
+        int[] from = new int[n];
+        Arrays.fill(from, -1);
+
+        int k = in.nextInt();
+        for (int i = 0; i < k ; i++) {
+            int v = in.nextInt()-1;
+            int va = in.nextInt();
+            add[v] = true;
+            num[v] = va;
+            q.add(new State(v, va));
+        }
+
+        boolean isOK = true;
+        while (q.size() >= 1) {
+            State st = q.poll();
+            for (int to : graph[st.now]) {
+                if (to == from[st.now]) {
+                    continue;
+                }
+
+                if (!add[to]) {
+                    add[to] = true;
+                    num[to] = st.value+1;
+                    from[to] = st.now;
+                    q.add(new State(to, st.value+1));
+                } else if (add[to]) {
+                    if (num[to] != st.value+1 && num[to] != st.value-1) {
+                        isOK = false;
+                    }
+                }
+            }
+        }
+
+        if (isOK) {
+            out.println("Yes");
+            for (int i = 0; i < n ; i++) {
+                out.println(num[i]);
+            }
+        } else {
+            out.println("No");
+        }
+        out.flush();
+    }
+
+    static class State implements Comparable<State> {
+        int now;
+        int value;
+
+
+        public State(int n, int v) {
+            now = n;
+            value = v;
+        }
+
+        @Override
+        public int compareTo(State o) {
+            return value - o.value;
+        }
+    }
+
+
+
+
+    static int[][] buildGraph(InputReader in, int n, int m) {
+        int[][] edges = new int[m][];
+        int[][] graph = new int[n][];
+        int[] deg = new int[n];
+        for (int i = 0 ; i < m ; i++) {
+            int a = in.nextInt()-1;
+            int b = in.nextInt()-1;
+            deg[a]++;
+            deg[b]++;
+            edges[i] = new int[]{a, b};
+        }
+        for (int i = 0 ; i < n ; i++) {
+            graph[i] = new int[deg[i]];
+        }
+        for (int i = 0 ; i < m ; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            graph[a][--deg[a]] = b;
+            graph[b][--deg[b]] = a;
+        }
+        return graph;
+    }
+
+
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int[] nextInts(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextInt();
+            }
+            return ret;
+        }
+
+
+        private int[][] nextIntTable(int n, int m) {
+            int[][] ret = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextInt();
+                }
+            }
+            return ret;
+        }
+
+        private long[] nextLongs(int n) {
+            long[] ret = new long[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextLong();
+            }
+            return ret;
+        }
+
+        private long[][] nextLongTable(int n, int m) {
+            long[][] ret = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextLong();
+                }
+            }
+            return ret;
+        }
+
+        private double[] nextDoubles(int n) {
+            double[] ret = new double[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextDouble();
+            }
+            return ret;
+        }
+
+        private int next() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public char nextChar() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            if ('a' <= c && c <= 'z') {
+                return (char) c;
+            }
+            if ('A' <= c && c <= 'Z') {
+                return (char) c;
+            }
+            throw new InputMismatchException();
+        }
+
+        public String nextToken() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.append((char) c);
+                c = next();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public int nextInt() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public long nextLong() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            long sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public double nextDouble() {
+            return Double.valueOf(nextToken());
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+}
