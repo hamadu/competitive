f62bba1df9400a7a8945b1ac2d3d190f22292ddb
hamadu <dercll1020@gmail.com>
2016-12-18 12:15:46 +0900

    dwango2017 qual

---
+++ atcoder/other2017/dwango2017/qual/B.java
@@ -0,0 +1,208 @@
+package atcoder.other2017.dwango2017.qual;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class B {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        char[] c = in.nextToken().toCharArray();
+        int n = c.length;
+        int[][] dp = new int[n+1][3];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        dp[0][2] = 0;
+
+        int maxLevel = 0;
+        for (int i = 0; i <= n ; i++) {
+            for (int p = 0; p < 3 ; p++) {
+                if (dp[i][p] == -1) {
+                    continue;
+                }
+                int base = dp[i][p];
+                maxLevel = Math.max(maxLevel, base);
+                if (i == n) {
+                    continue;
+                }
+
+                // '2'
+                if (c[i] == '2' || c[i] == '?') {
+                    int tv = (p == 1) ? base+1 : 1;
+                    dp[i+1][0] = Math.max(dp[i+1][0], tv);
+                }
+
+                // '5'
+                if (c[i] == '5' || c[i] == '?') {
+                    int tv = (p == 0) ? base+1 : 0;
+                    dp[i+1][1] = Math.max(dp[i+1][1], tv);
+                }
+
+                if (c[i] != '2' && c[i] != '5') {
+                    int tv = 0;
+                    dp[i+1][2] = Math.max(dp[i+1][2], tv);
+                }
+            }
+        }
+
+        out.println(maxLevel / 2 * 2);
+        out.flush();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int[] nextInts(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextInt();
+            }
+            return ret;
+        }
+
+        private int[][] nextIntTable(int n, int m) {
+            int[][] ret = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextInt();
+                }
+            }
+            return ret;
+        }
+
+        private long[] nextLongs(int n) {
+            long[] ret = new long[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextLong();
+            }
+            return ret;
+        }
+
+        private long[][] nextLongTable(int n, int m) {
+            long[][] ret = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextLong();
+                }
+            }
+            return ret;
+        }
+
+        private double[] nextDoubles(int n) {
+            double[] ret = new double[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextDouble();
+            }
+            return ret;
+        }
+
+        private int next() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public char nextChar() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            if ('a' <= c && c <= 'z') {
+                return (char) c;
+            }
+            if ('A' <= c && c <= 'Z') {
+                return (char) c;
+            }
+            throw new InputMismatchException();
+        }
+
+        public String nextToken() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.append((char) c);
+                c = next();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public int nextInt() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public long nextLong() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            long sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public double nextDouble() {
+            return Double.valueOf(nextToken());
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+}
