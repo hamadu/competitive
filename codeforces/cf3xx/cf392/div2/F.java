420127bce81ee3a6176fe813682b0f856f50509e
hamadu <dercll1020@gmail.com>
2017-02-03 22:54:23 +0900

    arc068

---
+++ codeforces/cf3xx/cf392/div2/F.java
@@ -0,0 +1,216 @@
+package codeforces.cf3xx.cf392.div2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class F {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int l = in.nextInt();
+        int r = in.nextInt();
+
+        out.println(solve(n, l, r));
+        out.flush();
+    }
+
+    private static long solve(int n, int l, int r) {
+        long L = r - l + 1;
+        if (n >= 25) {
+            return 0;
+        }
+        if (n == 1) {
+            return L;
+        }
+        if (n == 2) {
+            return L * (L - 1);
+        }
+
+        long[] pow = new long[r+1];
+        for (int i = 1 ; i <= r ; i++) {
+            pow[i] = (long)Math.pow(i, n-1);
+            if (pow[i] > 1e7) {
+                break;
+            }
+        }
+
+        long ans = 0;
+        for (int a = 1 ; a <= r ; a++) {
+            if (pow[a] == 0) {
+                break;
+            }
+            for (int b = a+1 ; b <= r ; b++) {
+                if (pow[b] == 0) {
+                    break;
+                }
+                if (gcd(a, b) == 1) {
+                    long up = r / pow[b];
+                    long lw = (l-1) / pow[a];
+                    ans += Math.max(0, up-lw);
+                }
+            }
+        }
+        return ans * 2;
+    }
+
+    public static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a%b);
+    }
+
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int[] nextInts(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextInt();
+            }
+            return ret;
+        }
+
+        private int[][] nextIntTable(int n, int m) {
+            int[][] ret = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextInt();
+                }
+            }
+            return ret;
+        }
+
+        private long[] nextLongs(int n) {
+            long[] ret = new long[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextLong();
+            }
+            return ret;
+        }
+
+        private long[][] nextLongTable(int n, int m) {
+            long[][] ret = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    ret[i][j] = nextLong();
+                }
+            }
+            return ret;
+        }
+
+        private double[] nextDoubles(int n) {
+            double[] ret = new double[n];
+            for (int i = 0; i < n; i++) {
+                ret[i] = nextDouble();
+            }
+            return ret;
+        }
+
+        private int next() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public char nextChar() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            if ('a' <= c && c <= 'z') {
+                return (char) c;
+            }
+            if ('A' <= c && c <= 'Z') {
+                return (char) c;
+            }
+            throw new InputMismatchException();
+        }
+
+        public String nextToken() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.append((char) c);
+                c = next();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public int nextInt() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public long nextLong() {
+            int c = next();
+            while (isSpaceChar(c))
+                c = next();
+            long sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = next();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c-'0';
+                c = next();
+            } while (!isSpaceChar(c));
+            return res*sgn;
+        }
+
+        public double nextDouble() {
+            return Double.valueOf(nextToken());
+        }
+
+        public boolean isSpaceChar(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+}
